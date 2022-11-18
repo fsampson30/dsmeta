51776cbad8fc325ac60e8cb7a95647b44f20d55d
fsampson30 <fsampson30@gmail.com>
2022-11-18 07:33:55 -0300

    Version correction.

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -20,7 +20,7 @@ public class SaleService {
 
     public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 
-        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+        LocalDate today = LocalDate.now();
 
         LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
         LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
